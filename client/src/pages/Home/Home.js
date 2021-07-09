b14be12202f1f238f4db5f4ce1575c518a2ca235
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-08 17:55:27 -0700

    Adding home page for later implementation

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Home;
