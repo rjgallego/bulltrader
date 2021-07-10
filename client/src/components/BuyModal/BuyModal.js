c9d1f159c0c270880575c8b8bf310cf816374389
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-09 19:58:36 -0700

    Renaming folder and ocmponent to BuyModal instead of SellModal as window is used for buying stocks

---
+++ client/src/components/BuyModal/BuyModal.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Modal, Button, Form} from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+
+const BuyModal = () => {
+    const dispatch = useDispatch();
+
+    const handleClose = () => {
+        dispatch({
+            type: 'HIDE'
+        })
+    }
+    const show = useSelector(state => state.buyReducer);
+    
+    return (
+        <Modal show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton>
+                <Modal.Title>Buy Stock</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control type="text" size='sm' placeholder="Search by name or symbol" />
+                    <Form.Control type="text" size='sm' placeholder="Number of Shares" />
+                </Form>
+
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="success" onClick={handleClose}>
+                    Buy
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default BuyModal;
