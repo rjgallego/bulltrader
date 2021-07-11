377e791fdcfdbad791532516baf607038b78e5ec
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-10 18:32:11 -0700

    Importing AccountModel and using to create new user account on registration

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import UserModel
+from .models import UserModel, AccountModel
 from backend import db
 from flask_jwt_extended import create_access_token, jwt_required
 
@@ -32,9 +32,13 @@ def register():
             )
 
         hash = generate_password_hash(password, method='sha256')
-        user = UserModel(firstname, lastname, email, hash)
+        user = UserModel(firstname=firstname, lastname=lastname, email=email, hash=hash)
+        account = AccountModel(user=user, balance=100000.00)
+        
         db.session.add(user)
+        db.session.add(account)
         db.session.commit()
+
         return jsonify(
             success="User added to database"
         )
