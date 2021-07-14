48bd5bc9b2f253fac2d09c91620a519eedb7d040
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-13 22:01:02 -0700

    Adding state and error message for form, and adding route to enable login functionality from the server

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
-import {Container, Row, Col, Form, Button} from 'react-bootstrap';
-import ImgCarousel from './ImgCarousel';
-import NavBar from '../../components/NavBar/NavBar';
-import './Home.css';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap'
+import ImgCarousel from './ImgCarousel'
+import NavBar from '../../components/NavBar/NavBar'
+import './Home.css'
+import axios from 'axios'
+import {useDispatch} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+
 
 const Home = () => {
+    const dispatch = useDispatch()
+
     const [validated, setValidated] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage,setErrorMessage] = useState(null);
+    const [reroute, setReroute] = useState(false)
+    const handleEmail = (event) => setEmail(event.target.value)
+    const handlePassword = (event) => setPassword(event.target.value)
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -18,7 +30,29 @@ const Home = () => {
             setValidated(true);
             return
         }
+
+        const data = {
+            email: email,
+            password: password
+        }
+
+        axios.post('/api/login', data)
+            .then(response => {
+                if(response.data.error){
+                    setErrorMessage(response.data.error)
+                    return
+                } 
+
+                sessionStorage.setItem("token", response.data.token)
+                setReroute(true)
+
+            })
     }
+
+    if(reroute){
+        return <Redirect to="/dashboard" />
+    }
+    
     return (
         <div>
             <NavBar/>
@@ -33,10 +67,11 @@ const Home = () => {
                                 className='bg-light h-100 d-flex flex-column justify-content-center'>
                                 <h2 className='text-center mb-5'>Welcome to Bull Trader</h2>
                                 <h5 className='text-center mb-4'>Please Sign In</h5>
-                                <Form.Control required className="w-75 mx-auto mb-3" type="email" placeholder="Enter email" />
-                                <Form.Control required className="w-75 mx-auto mb-4" type="password" placeholder=" Enter password" />
+                                { errorMessage ? <p className="text-center text-danger">{errorMessage}</p> : ''}
+                                <Form.Control required className="w-75 mx-auto mb-3" type="email" placeholder="Enter email" onChange={handleEmail}/>
+                                <Form.Control required className="w-75 mx-auto mb-4" type="password" placeholder=" Enter password" onChange={handlePassword}/>
                                 <Form.Group as={Row} className="d-flex flex-column">
-                                    <Button className="w-50 mx-auto mb-2" variant="success" type="submit">Home</Button>
+                                    <Button className="w-50 mx-auto mb-2" variant="success" type="submit">Login</Button>
                                     <Button href="/register" className="w-50 mx-auto" variant="success">Register</Button>
                                 </Form.Group>
                             </Form>
