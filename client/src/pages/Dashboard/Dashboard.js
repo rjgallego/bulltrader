4c990a4a8067e2db9d0f29d03bd145e48f24cdf6
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-13 21:59:02 -0700

    removing test data, adding routes for pulling data from backedn, and adding separate returned views for different scenarios

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,65 +1,78 @@
-import React, {useState} from 'react';
-import NavBar from '../../components/NavBar/NavBar';
-import SideBar from '../../components/SideBar/SideBar';
-import StockTable from './StockTable';
-import BuyModal from '../../components/BuyModal/BuyModal';
-import { Container, Row, Col, Button} from 'react-bootstrap';
-import Chart from 'react-google-charts';
-import {useDispatch} from 'react-redux';
+import React, {useState, useEffect} from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+import SideBar from '../../components/SideBar/SideBar'
+import StockTable from './StockTable'
+import StockChart from './StockChart'
+import BuyModal from '../../components/BuyModal/BuyModal'
+import { Container, Row, Col, Button, Spinner} from 'react-bootstrap'
+import jwt_decode from 'jwt-decode'
+import axios from 'axios'
+import {useDispatch, useSelector} from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 import './Dashboard.css'
 
-const data = {
-    firstName: "Joe",
-    lastName: "Smith",
-    value: 123384.96,
-    cash: 890000.00,
-    stocks: [ {
-            symbol: "AAPL",
-            name: "Apple Inc.",
-            shares: 73,
-            price: 143.24,
-            value: 10553.61,
-            increase: true
-        }, {
-            symbol: "TEST",
-            name: "Test Inc.",
-            shares: 15,
-            price: 25.24,
-            value: 378.6,
-            increase: false
-        }, {
-            symbol: "TEST",
-            name: "Test Inc.",
-            shares: 15,
-            price: 25.24,
-            value: 378.6,
-            increase: false
-        }, {
-            symbol: "TEST",
-            name: "Test Inc.",
-            shares: 15,
-            price: 25.24,
-            value: 378.6,
-            increase: false
-        }
-    ]
-}
-
 const Dashboard = () => {
     const dispatch = useDispatch()
+    const user = useSelector(state => state.userReducer)
+    const [reroute, setReroute] = useState(false)
+    //const [token, setToken] = useState(token)
 
-    const createChartData = () => {
-        const chartData = []
-        chartData[0] = ['Stocks', 'Percent of Total']
-        data.stocks.forEach(stock => chartData.push([stock.name, stock.shares]))
-        return chartData
+    useEffect(() => {
+        setUserInfo()
+    },[])
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    const setUserInfo = () => {
+        const token = sessionStorage.getItem('token')
+        const userId = jwt_decode(token).sub
+
+        const options = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        axios.get(`/api/stocks/${userId}`, options)
+            .then(response => {
+                dispatch({
+                    type: 'SET_USER',
+                    payload: response.data
+                })
+            }).catch(error => {
+                if(error.response.status == 401) setReroute(true)
+            })
     }
 
     const handleClick = () => {
         dispatch({
             type: 'SHOW'
         })
+        // dispatch({
+        //     type: 'UPDATE_USER_STOCKS',
+        //     payload: data.stocks
+        // })
+    }
+
+    if(!sessionStorage.getItem('token')  || reroute){
+        return <Redirect to='/' /> 
+    }
+
+    if(!user){
+        return (
+            <>
+                <NavBar />
+                <div className="h-100 w-100 d-flex justify-content-center align-items-center">
+                    <Spinner animation="border" 
+                            role="status" 
+                            variant="light"/>
+                </div>
+            </>
+        )
     }
 
     return (
@@ -68,18 +81,18 @@ const Dashboard = () => {
             <SideBar />
             <Container id="dashboard-container" className="position-fixed overflow-hidden pt-5 text-light ml-5">
                 <Row className="d-flex flex-column ml-3">
-                    <h1 className="mb-0">{data.value}</h1>
+                    <h1 className="mb-0">{formatter.format(user.user.value)}</h1>
                     <p className="mt-0">Portfolio Value</p>
                 </Row>
                 <Row lg={2} className="w-100">
                     <Col>
                         <div id="table-div">
-                            <StockTable stocks={data.stocks} />
+                            <StockTable />
                         </div>
                         <Row lg={2}>
                             <Col className="text-center">
                                 <p className="mb-0 mt-3">Available Cash</p>
-                                <h2 className="mt-0">{data.cash}</h2>
+                                <h2 className="mt-0">{formatter.format(user.user.balance)}</h2>
                             </Col>
                             <Col className="text-center d-flex align-items-center">
                                 <Button variant="success" className="w-75 h-75" onClick={handleClick}>Buy</Button>
@@ -87,30 +100,7 @@ const Dashboard = () => {
                         </Row>
                     </Col>
                     <Col>
-                        <Chart 
-                            className="bg-dark"
-                            width={'100%'}
-                            height={'70vh'}
-                            chartType='PieChart'
-                            loader={<div>Loading Chart</div>}
-                            data={
-                                createChartData()
-                            }
-                            options={{
-                                title: 'Portfolio Summary',
-                                pieHole: 0.3,
-                                animation: {
-                                    startup: true,
-                                    easing: 'linear',
-                                    duration: 1500,
-                                  },
-                                  colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-                                  backgroundColor: '000000',
-                                  titleTextStyle: {italic: false, bold: true, color: 'FFFFFF', fontSize:'30px'},
-                                  legend: {position: 'none'},
-                                  baselineColor: 'FFFFFF',
-                            }}
-                        />
+                        <StockChart />
                     </Col>
                 </Row>
             </Container>
