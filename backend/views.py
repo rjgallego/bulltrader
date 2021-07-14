be348ce6735cd6581a5017fe1fdfb7f8889e90e1
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-14 15:06:27 -0700

    updating /buy endpoint to search account buy user_id instead of account id

--- backend/views.py
+++ backend/views.py
@@ -55,9 +55,9 @@ def buy():
     if request.method == "POST":
         symbol = request.json["symbol"]
         shares = request.json["shares"]
-        acct_id = request.json["acct_id"]
+        user_id = request.json["user_id"]
 
-        account = AccountModel.query.filter_by(id=acct_id).first()
+        account = AccountModel.query.filter_by(user_id=user_id).first()
         if account == None:
             return jsonify(
                 error="Account does not exist"
@@ -66,7 +66,7 @@ def buy():
         account.balance = account.balance - decimal.Decimal(get_stock_value(symbol, shares))
         db.session.commit()
 
-        exists = StockModel.query.join(StockModel.account).filter(AccountModel.id==acct_id, StockModel.symbol==symbol).first()
+        exists = StockModel.query.join(StockModel.account).filter(AccountModel.id==account.id, StockModel.symbol==symbol).first()
         if exists != None:
             exists.shares = exists.shares + shares
             db.session.commit()
