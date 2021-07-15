fc45887bd834b14de3497a5f7f3cc829f5918431
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-14 19:21:56 -0700

    Adding styling and dispatch for closing Modal window

--- client/src/pages/Dashboard/StockTable.js
+++ client/src/pages/Dashboard/StockTable.js
@@ -1,48 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Table, Button} from 'react-bootstrap'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
-const StockTable = () => {
+const StockTable = ({setStockToSell}) => {
+    const dispatch = useDispatch()
     const stocks = useSelector(state => state.userReducer.user.stocks)
-
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
     });
 
+    const handleClick = (event) => {
+        setStockToSell(stocks[event.target.id])
+        dispatch({
+            type: 'SHOW_SELL'
+        })
+    }
+
     const createRows = () => {
-        return stocks.map(stock => {
+        return stocks.map((stock, i) => {
             return <tr>
-                    <td>{stock.name}</td>
+                    <td className="overflow-hidden" 
+                        style={{whiteSpace: 'nowrap'}}>{stock.name}</td>
                     <td>{stock.symbol}</td>
                     <td>{stock.shares}</td>
                     <td>{formatter.format(stock.price)}</td>
                     <td>{formatter.format(stock.value)}</td>
                     <td>
-                        <Button variant="light" size='sm' className="px-3">Sell</Button>
+                        <Button id={i} variant="light" 
+                                size='sm' 
+                                className="px-3"
+                                onClick={handleClick}>Sell</Button>
                     </td>
                 </tr>
         })
     }
 
     return (
-        <Table variant="dark" size={'sm'}>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Symbol</th>
-                    <th>Shares</th>
-                    <th>Price</th>
-                    <th>Total Value</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    createRows()
-                }
-            </tbody>
-        </Table>
+            <Table variant="dark" size={'sm'} style={{tableLayout : 'fixed'}}>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Symbol</th>
+                        <th>Shares</th>
+                        <th>Price</th>
+                        <th>Total Value</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        createRows()
+                    }
+                </tbody>
+            </Table>
+
     )
 }
 
