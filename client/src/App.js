5b6882957dbeb9ec61e091c5b6f29e4eaa8be3f8
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-07-16 12:36:36 -0700

    Adding stock summary element and page to the apps Browser Router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Route, BrowserRouter} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
 import Dashboard from './pages/Dashboard/Dashboard'
+import StockSummary from './pages/StockSummary/StockSummary'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home}/>
         <Route path="/register" component={Register}/>
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/stock-summary/:symbol" component={StockSummary} />
       </BrowserRouter>
     </div>
   );
